3e4a4b371b888531cde4a31731c4add70934ced8
usman maqbool <usmanmaqbool636@gmail.com>
2020-04-23 10:00:38 +0500

    build

---
+++ build/precache-manifest.593bad9e976e11e6708c3b27c7dd16b1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb21f044157673c4dc70b5538124187f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c8d44ee458768e97aad0",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "36e75751e40f69941668",
+    "url": "/static/js/2.bf062c3a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.bf062c3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8d44ee458768e97aad0",
+    "url": "/static/js/main.682a8668.chunk.js"
+  },
+  {
+    "revision": "700d31d1c6f37b7bdc51",
+    "url": "/static/js/runtime-main.84578d5e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
